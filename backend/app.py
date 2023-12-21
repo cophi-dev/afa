6715a4269679752bb7747887e825420da0817a41
cophi <pzarindast@gmail.com>
2023-12-21 18:04:46 +0000

    changed max-width

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,7 @@ CORS(app, resources={r"/api/*": {"origins": ["https://afa-editor.vercel.app", "h
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'traits'))
 special_assets = {
     'bape_coach': os.path.join(base_dir, 'memes', 'bape_coach.png'),
-    'bape_bape': os.path.join(base_dir, 'memes', 'bape_shirt.png'),
+    'bape_shirt': os.path.join(base_dir, 'memes', 'bape_shirt.png'),
     'jacket': os.path.join(base_dir, 'memes', 'jacket.png'),
     'bape_hoodie_red': os.path.join(base_dir, 'memes', 'bape_hoodie_red.png'),
     'bape_hoodie_green': os.path.join(base_dir, 'memes', 'bape_hoodie_green.png'),
