ca445ed86ced688fc3b025775656350f56e3b369
cophi <pzarindast@gmail.com>
2023-12-30 10:47:58 +0000

    added new silvester traits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -244,7 +244,7 @@ function App() {
                     <h3 className="dropdown-header">Hat</h3>
                     <select value={hatAsset} onChange={handleHatAssetChange} className="dropdown" disabled={!tokenId}>
                         <option value="">Select</option>
-                        <option value="glitter_cowboy">Glitter Cowboy Hat</option>
+                        <option value="glitter_cowboy_hat">Glitter Cowboy Hat</option>
                         <option value="christmas_hat">Christmas Hat</option>
                         <option value="christmas_hat2">Christmas Hat 2</option>
                         <option value="christmas_hat3">Christmas Hat 3</option>
