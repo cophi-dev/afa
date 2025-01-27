74a46839837d284332f5bb12b40da2d860027d57
cophi <pzarindast@gmail.com>
2025-01-27 22:01:42 +0100

    added mint status check

--- frontend/src/Loader.js
+++ frontend/src/Loader.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './Loader.css';  // Assuming you will create a Loader.css for styling
 
-function Loader({ backgroundColor }) {
-    return (
-        <div className="loader" style={{ backgroundColor }}>
-            Waiting for your Ape to change their perspective...
-        </div>
-    );
-}
+const Loader = () => (
+  <div className="loader">
+    <div className="spinner"></div>
+  </div>
+);
 
 export default Loader;
